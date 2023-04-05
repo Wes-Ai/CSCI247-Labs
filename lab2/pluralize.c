eb140b6943bc2a38ee4798d4a879e83a2fafd091
Wesley Smith <wesy32@gmail.com>
2023-04-05 12:30:40 -0700

    Lab2: 3-endings workinggit add .

--- lab2/pluralize.c
+++ lab2/pluralize.c
@@ -5,41 +5,39 @@
 
 
 bool ends_with(const char* s, const char* t) {  // s = big word
-    int len = strlen(s);
-    puts ("here0");
-    char * compare;
-    compare = strstr (s, "ife");
-    puts ("here1");
-    if (compare != NULL) {
-        strncpy (compare, "ive", len);
-    }
-    puts ("here2");
+    // Set up indexing variables
+    int compareLength = strlen(t);
+    int wordLength = strlen(s);
+    int lastIndex = wordLength - 1;
 
-    return true;
+    char oneChar[20];
+    char twoChar[20];
+    char threeChar[20];
+
+    if(compareLength == 3) {
+        strcpy(threeChar, s + (lastIndex - 2)); //Copy the last 3 chars of the input word
+        if (strcmp(threeChar, t) == 0) {  // Compare last 3 of word with given ending
+            return true;    // They match!
+        }
+        else {
+            return false;   // No match!
+        }
+    }
+    else if (compareLength == 2) {
+        strcpy(twoChar, s + (lastIndex - 1)); //Copy the last 3 chars of the input word
+        if (strcmp(threeChar, t) == 0) {  // Compare last 3 of word with given ending
+            return true;    // They match!
+        }
+        else {
+            return false;   // No match!
+        }
+    }
+    else if (compareLength == 1) {
+        return true;
+    }
 }
 
 void main() {
-
-    // * * * * * * * * * * * * * * * * * * * * * * * * *
-
-    // char str[] ="Strife";
-    // char * pch;
-    // pch = strstr (str,"ife");
-    // puts (pch);
-    // int len = strlen(str);
-    // if (pch != NULL)
-    //     strncpy (pch,"ive", len);
-    // puts (str);
-
-    // * * * * * * * * * * * * * * * * * * * * * * * * *
-
-
-    // char string[11] = "Hello World";
-
-    // int length = strlen(string);
-    // int index = length - 1;
-
-    // printf("Last: %c\n", string[index]);
 
     char ife[] = "ife";
     char oneChar[20];
@@ -55,14 +53,16 @@ void main() {
     printf("Enter word: ");
     scanf("%s", word);
 
-    // Set up indexing variables
-    int length = strlen(word);
-    int lastIndex = length - 1;
+    int wordLength = strlen(word);
+
+
+
+    
 
     // Set up end-of-word comparison strings
-    strcpy(threeChar, word + (lastIndex - 2));
-    strcpy(twoChar, word + (lastIndex - 1));
-    strcpy(oneChar, word + (lastIndex));
+    // strcpy(threeChar, word + (lastIndex - 2));
+    // strcpy(twoChar, word + (lastIndex - 1));
+    // strcpy(oneChar, word + (lastIndex));
 
     // Testing to see variables are printed correctly
     // printf("Last 3: %s\n", threeChar);
@@ -74,14 +74,27 @@ void main() {
 
     char * pch;
 
+    char temp[20];
+
     // Level 1
     if (num == 1) {
         printf("%d %s\n", num, word);
     }
-    else if (strcmp(threeChar, ife) == 0) {
-        strncpy(returnWord, word, (lastIndex - 2)); //TODO: strncpy instead of strcpy!!!!!
-        printf("%d %s\n", num, returnWord);
+    else if (ends_with(word, ife)) {
+        strncpy(temp, word, (wordLength - 3));  // Copy the word minus the ending to temp. "Strife" -> "Str"
+        strcat(temp, "ives");   // Build the final word w/ ending, "Str" -> "Strives"
+        printf("%d %s\n", num, temp);
     }
+
+
+
+    // else if (strcmp(threeChar, ife) == 0) {
+    //     strncpy(returnWord, word, (lastIndex - 2)); //TODO: strncpy instead of strcpy!!!!!
+    //     printf("%d %s\n", num, returnWord);
+    // }
+
+
+
     // else if (strstr(word, "ife")) {
     //     int len = strlen(word);
     //     pch = strstr(word, "ife");
