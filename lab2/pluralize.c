7fd8a6f722e3fc8fc9eb00a645897b0a62d88a6c
Wesley <smithw26@wwu.edu>
2023-04-05 11:45:53 -0700

    Added variables for end-of-word comparisons.

--- lab2/pluralize.c
+++ lab2/pluralize.c
@@ -33,29 +33,60 @@ void main() {
 
     // * * * * * * * * * * * * * * * * * * * * * * * * *
 
+
+    // char string[11] = "Hello World";
+
+    // int length = strlen(string);
+    // int index = length - 1;
+
+    // printf("Last: %c\n", string[index]);
+
+    char ife[] = "ife";
+    char oneChar[20];
+    char twoChar[20];
+    char threeChar[20];
+
+    // User input
     int num;
     printf("Enter number: ");
     scanf("%d", &num);
-
     char word[25];
     printf("Enter word: ");
     scanf("%s", word);
-    
+
+    // Set up indexing variables
+    int length = strlen(word);
+    int lastIndex = length - 1;
+
+    // Set up end-of-word comparison strings
+    strcpy(threeChar, word + (lastIndex - 2));
+    strcpy(twoChar, word + (lastIndex - 1));
+    strcpy(oneChar, word + (lastIndex));
+
+    // Testing to see variables are printed correctly
+    // printf("Last 3: %s\n", threeChar);
+    // printf("Last 2: %s\n", twoChar);
+    // printf("Last 1: %s\n", oneChar);
+
+
     char s = 's';
+
     char * pch;
 
     // Level 1
     if (num == 1) {
         printf("%d %s\n", num, word);
     }
-    else if (strstr(word, "ife")) {
-        int len = strlen(word);
-        pch = strstr(word, "ife");
-        if(pch != NULL) {
-            strncpy(pch, "ives", len + 1);
-        }
-        printf("%d %s\n", num, word);
+    else if (strcmp(threeChar, ife) == 0) {
+        printf("Poggers.");
     }
+    // else if (strstr(word, "ife")) {
+    //     int len = strlen(word);
+    //     pch = strstr(word, "ife");
+    //     if(pch != NULL) {
+    //         strncpy(pch, "ives", len + 1);
+    //     }
+    //     printf("%d %s\n", num, word);
     else {
         strncat(word, &s, 1);
         printf("%d %s\n", num, word);
